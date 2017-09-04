414b2ca44164a7adae44272f4c650eca2067b4b7
Manuel Abrante <abrantemanuel@gmail.com>
2017-09-03 22:38:53 -0400

    Update main style and base layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
+// providers
 import { ApiRequestsService } from './api/requests.service';
-
+// components
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/'
 // config settings
 import { Config } from './app.config';
 import { _Config } from './config';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
