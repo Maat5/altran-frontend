abb86fdd95937fbf0fd07745d733bd415abd115b
Manuel Abrante <abrantemanuel@gmail.com>
2017-09-04 18:17:08 -0400

    display gnome information

--- src/app/gnomes/details/details.component.ts
+++ src/app/gnomes/details/details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { GnomesService } from '../gnomes.service';
+import { GnomeInterface } from '../../interfaces';
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  gnome: GnomeInterface;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private gnomesService: GnomesService,
+  ) { }
 
   ngOnInit() {
+
+    this.gnome = this.gnomesService.profile;
+    // validate if route id is the same as data stored
+    this.route.params.subscribe((params: Params) =>  {
+      if(params.id !== this.gnome.id.toString())
+        this.router.navigate(['gnomes/']);
+    });
   }
 
 }
