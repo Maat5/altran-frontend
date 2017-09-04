c6c78e1ce3cc823097c341ddcc48cd3f317e397b
Manuel Abrante <abrantemanuel@gmail.com>
2017-09-03 21:45:19 -0400

    Make request example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { ApiRequestsService } from './api/requests.service';
+import { Config } from  './app.config';
+import { ConfigInterface, GnomeInterface } from './interfaces';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(
+    @Inject(Config) private config: ConfigInterface,
+    private api: ApiRequestsService
+  ){}
+
+  ngOnInit() {
+    this.api.get(this.config.apiUrl)
+      .subscribe(
+        data  => console.log('data', data),
+        error => console.log('error', error)
+      )
+  }
 }
